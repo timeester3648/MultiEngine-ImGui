12ff01fd47e0864c0edbbb3f5326a819be735a17
timeester3648 <weteringt@gmail.com>
2022-07-05 22:41:27 +0200

    Update submodule

--- imconfig.h
+++ imconfig.h
@@ -60,6 +60,7 @@
 //---- Unless IMGUI_DISABLE_DEFAULT_FORMAT_FUNCTIONS is defined, use the much faster STB sprintf library implementation of vsnprintf instead of the one from the default C library.
 // Note that stb_sprintf.h is meant to be provided by the user and available in the include path at compile time. Also, the compatibility checks of the arguments and formats done by clang and GCC will be disabled in order to support the extra formats provided by STB sprintf.
 #define IMGUI_USE_STB_SPRINTF
+#define IMGUI_STB_SPRINTF_FILENAME <stb/stb_sprintf.h>
 
 
 //---- Use 32-bit vertex indices (default is 16-bit) is one way to allow large meshes with more than 64K vertices.
