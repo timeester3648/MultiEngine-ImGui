b8deaf9dfa875af419e549d2e8d2e20ec6d481d2
Tim van de Wetering <45339455+timeester3648@users.noreply.github.com>
2022-12-07 15:23:00 +0100

    Update submodule

--- premake5.lua
+++ premake5.lua
@@ -34,24 +34,7 @@ project "ImGui"
 	}
 
  	filter "system:windows"
-		disablewarnings { "4267", "4996" }
 		includedirs { "%{IncludeDir.general_includes}/windirent" }
 
- 	filter "configurations:Debug"
-		defines { "MLE_DEBUG_BUILD", "DEBUG" }
-		runtime "Debug"
-		symbols "on"
-
-	filter "configurations:Release"
-		defines { "MLE_RELEASE_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
-
-	filter "configurations:Distribution"
-		defines {  "MLE_DISTRIBUTION_BUILD", "NDEBUG" }
-		flags { "LinkTimeOptimization" }
-		runtime "Release"
-		optimize "speed"
-		intrinsics "on"
+	filter "action:vs*"
+		disablewarnings { "4267", "4996" }
