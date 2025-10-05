2a1cff08bf3f9ccc91ab1a59a742667265b62f1e
timeester3648 <weteringt@gmail.com>
2025-10-05 19:28:18 +0200

    Update

--- imconfig.h
+++ imconfig.h
@@ -67,7 +67,7 @@
 //#define IMGUI_USE_LEGACY_CRC32_ADLER
 
 //---- Use 32-bit for ImWchar (default is 16-bit) to support Unicode planes 1-16. (e.g. point beyond 0xFFFF like emoticons, dingbats, symbols, shapes, ancient languages, etc...)
-//#define IMGUI_USE_WCHAR32
+#define IMGUI_USE_WCHAR32
 
 //---- Avoid multiple STB libraries implementations, or redefine path/filenames to prioritize another version
 // By default the embedded implementations are declared static and not available outside of Dear ImGui sources files.
@@ -156,9 +156,6 @@
 
 //---- Debug Tools: Enable slower asserts
 //#define IMGUI_DEBUG_PARANOID
-
-#define IMGUI_USE_WCHAR32
-#define IMGUI_ENABLE_FREETYPE
 
 #include <MultiEngine/gui/imgui/ImGuiContext.h>
 
